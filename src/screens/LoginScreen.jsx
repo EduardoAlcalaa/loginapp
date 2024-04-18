0adde5b954c67c7144c252295339db9649b9b756
EduardoAlcalaa <eduardoantunes755@gmail.com>
2024-04-18 16:52:38 -0300

    gg3

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
+import { styles } from "../config/styles";
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState("");
@@ -10,24 +11,49 @@ export default function LoginScreen({ navigation }) {
     console.log("Fazer Login");
   }
 
+ 
   return (
-    <View>
-      <Text>Faça seu Login</Text>
+    
+    <View style={styles.container}>
+      <View style={styles.container}>
+
+      <Image
+        source={require("../../assets/loginicons.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text
+      variant="headlineMedium"
+      style={{
+        textAlign: "center",
+        marginBottom: 20,
+      }}
+
+      >Faça seu Login
+      </Text>
       <TextInput
         placeholder="Digite seu e-mail"
         onChangeText={setEmail}
         value={email}
+        style={styles.input}
       />
       <TextInput
         placeholder="Digite sua senha"
         onChangeText={setSenha}
         value={senha}
         secureTextEntry // faz com que o campo seja senha com *
+        style={styles.input}
       />
-      <Button onPress={realizaLogin}>Fazer Login</Button>
+      <View>
+      <Button onPress={realizaLogin} mode="contained">
+        Fazer Login
+      </Button>
+      </View>
+      
       <Button onPress={() => navigation.navigate("RegisterScreen")}>
         Faça seu cadastro
       </Button>
+      </View>
     </View>
+    
   );
 }
